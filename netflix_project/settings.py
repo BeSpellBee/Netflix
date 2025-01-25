c69b85b986ca1fd33692ba906c3c26d468639536
BeSpellBee <165915954+BeSpellBee@users.noreply.github.com>
2025-01-25 20:48:57 +0100

    changes

--- netflix_project/settings.py
+++ netflix_project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=%0v=6sj6bfbm0*@_=ry$0g5g@k86m=%@by8a5zmfv(hy1f(n^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['bespellbee.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
